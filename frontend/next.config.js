82fa7cc3f5876babbaaf43f2ec8b4744f3efb0cb
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-05-17 19:37:56 -0400

    feat: create frontend and add configurations

---
+++ frontend/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+const withImages = require('next-images');
+
+module.exports = nextConfig, withImages({
+  esModule: true,
+});
